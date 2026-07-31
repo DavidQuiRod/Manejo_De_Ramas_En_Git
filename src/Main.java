7f513b2b7489902d4c59690089677337c0eb656f
unknown <david.quiroz.dqr.jpg@gmail.com>
2026-07-31 11:18:51 -0600

    Se agrego la funcion operacionResta a la rama de david

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,4 @@
 import java.awt.*;
-
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
@@ -9,6 +8,7 @@ public class Main {
         System.out.println("Hello and welcome!");
         System.out.println(mensajeDeBienvenida("David"));
         System.out.println(operacionSuma(2,5));
+        System.out.println(funcionesClass.operacionResta(19,8));
     }
     public static String mensajeDeBienvenida(String nombre){
         return "Hola "+nombre +" bienvenido";
