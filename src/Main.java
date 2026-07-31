3ceb653be5d4ba190eb2e28277a5b38f348ad877
unknown <david.quiroz.dqr.jpg@gmail.com>
2026-07-31 10:59:06 -0600

    Carga de cambios desde ramaDavid

--- src/Main.java
+++ src/Main.java
@@ -8,14 +8,14 @@ public class Main {
         // to see how IntelliJ IDEA suggests fixing it.
         System.out.println("Hello and welcome!");
         System.out.println(mensajeDeBienvenida("David"));
-
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+        System.out.println(operacionSuma(2,5));
     }
     public static String mensajeDeBienvenida(String nombre){
         return "Hola "+nombre +" bienvenido";
+    }
+
+    public static String operacionSuma(int num1, int num2){
+        int res= num1+num2;
+        return  "El resultado de la sumas es "+ res;
     }
 }
