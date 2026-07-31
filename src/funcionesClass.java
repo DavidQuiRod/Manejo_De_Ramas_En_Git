c2bc80feaf08d08819f5c75e2fe86be5df30bef1
unknown <david.quiroz.dqr.jpg@gmail.com>
2026-07-31 12:36:33 -0600

    Se agrego un live template para agregar el autor de la funcion

--- src/funcionesClass.java
+++ src/funcionesClass.java
@@ -1,6 +1,25 @@
 public class funcionesClass{
   //Ejemplo de clase
+
+    /**
+     * 31/07/2026
+     * Nombre: operacionResta
+     * @autor: David Quiroz 
+     * 
+     */
     public static String operacionResta(int num1, int num2){
+
         return "El resultado de la resta es: "+ (num1-num2);
+    }
+
+    public static String operacionMultiplicacion(int num1, int num2){
+        return "Resultado";
+    }
+
+    public static  String operacionDivision(int num1, int num2){
+        return "Hola";
+    }
+    public static String ejemplo2(String Rutas){
+        return "hola";
     }
 }
